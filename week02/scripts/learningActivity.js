448d6419de5c1f62b3c70acf014695a6b9c3dcab
murilolhernandes <murilo.luiz.hernandes@gmail.com>
2025-01-15 14:55:53 -0700

    finished learning activities

---
+++ week02/scripts/learningActivity.js
@@ -0,0 +1,7 @@
+const hamburgerElement = document.querySelector("#myButton");
+const navElement = document.querySelector("#animateme");
+
+hamburgerElement.addEventListener("click", () => {
+  navElement.classList.toggle("open");
+  hamburgerElement.classList.toggle("open");
+});

1083115a715a8aace5d7ef9a09c624afdc6cfd89
murilolhernandes <murilo.luiz.hernandes@gmail.com>
2025-02-13 18:55:34 -0700

    going well so far

--- project/scripts/recipes.js
+++ project/scripts/recipes.js
@@ -63,16 +63,19 @@ const card = document.querySelector(".cards");
 async function getRecipes() {
   const response = await fetch(recipes);
   const data = await response.json();
-  console.log(data);
+  // console.log(data);
   displayRecipes(data.recipes);
 }
 
 getRecipes();
 
 const displayRecipes = (recipes) => {
+  let recipeIndex = 0;
+
   for (const category in recipes) {
     recipes[category].forEach((recipe) => {
       let section = document.createElement("section");
+      section.id = `recipe-${recipeIndex}`;
 
       let recipeName = document.createElement("h2");
       let image = document.createElement("img");
@@ -98,6 +101,12 @@ const displayRecipes = (recipes) => {
       let yieldDiv = document.createElement("div");
       let yield = document.createElement("h3");
 
+      let buttonDiv = document.createElement("div");
+      let button = document.createElement("button");
+
+      let recipeInfoDiv = document.createElement("div");
+      let reviewInfoDiv = document.createElement("div");
+
       recipeName.textContent = `${recipe.name}`;
       image.setAttribute("src", recipe.imgURL);
       image.setAttribute("alt", `${recipe.name} recipe image`);
@@ -112,6 +121,21 @@ const displayRecipes = (recipes) => {
       totalTime.textContent = `Total Time: ${recipe.totalTime}`;
       servings.textContent = `Servings: ${recipe.serving}`;
       yield.textContent = `Yield: ${recipe.yield || 'N/A'}`;
+      button.textContent = "View Recipe";
+
+      section.classList.add("card-section");
+      recipeDetailsDiv.classList.add("recipe-details");
+      prepTimeDiv.classList.add("prep-time");
+      cookTimeDiv.classList.add("cook-time");
+      addTimeDiv.classList.add("add-time");
+      addTimeDiv.classList.add("add-time");
+      totalTimeDiv.classList.add("total-time");
+      servingsDiv.classList.add("servings");
+      yieldDiv.classList.add("yield");
+      buttonDiv.classList.add("button-div");
+      button.classList.add("button");
+      recipeInfoDiv.classList.add("recipe-info");
+      reviewInfoDiv.classList.add("review-info");
 
       prepTimeDiv.appendChild(prepTime);
       cookTimeDiv.appendChild(cookTime);
@@ -119,12 +143,15 @@ const displayRecipes = (recipes) => {
       totalTimeDiv.appendChild(totalTime);
       servingsDiv.appendChild(servings);
       yieldDiv.appendChild(yield);
+      buttonDiv.appendChild(button);
       recipeDetailsDiv.appendChild(prepTimeDiv);
       recipeDetailsDiv.appendChild(cookTimeDiv);
       recipeDetailsDiv.appendChild(addTimeDiv);
       recipeDetailsDiv.appendChild(totalTimeDiv);
       recipeDetailsDiv.appendChild(servingsDiv);
       recipeDetailsDiv.appendChild(yieldDiv);
+      recipeDetailsDiv.appendChild(buttonDiv);
+      recipeDetailsDiv.appendChild(recipeInfoDiv);
       section.appendChild(recipeName);
       section.appendChild(image);
       section.appendChild(description);
@@ -132,8 +159,81 @@ const displayRecipes = (recipes) => {
       section.appendChild(recipeDetailsDiv);
 
       card.appendChild(section);
-    })
+      card.appendChild(reviewInfoDiv);
+
+      
+      button.addEventListener("click", () => {
+        if (recipeInfoDiv && reviewInfoDiv) {
+          recipeInfoDiv.innerHTML = "";
+          reviewInfoDiv.innerHTML = "";
+
+          displayDetails(recipe, recipeInfoDiv);
+          displayReviews(recipe, reviewInfoDiv);
+        }
+      });
+
+      recipeIndex++;
+    });
   }
+};
+
+const displayDetails = (recipe, container) => {
+  // console.log(`Displaying details for: ${recipe.name}`);
+
+  let knowMoreDiv = document.createElement("div");
+  let ingredientsDiv = document.createElement("div");
+  let directionsDiv = document.createElement("div");
+  let ingredientsList = document.createElement("ul");
+
+  recipe.ingredients.forEach((ingredient) => {
+    let li = document.createElement("li");
+    li.textContent = `${ingredient}`;
+    ingredientsList.appendChild(li);
+  });
+
+  recipe.steps.forEach((step) => {
+    let directions = document.createElement("p");
+    directions.textContent = `${step[0]}. ${step[1]}`;
+    directionsDiv.appendChild(directions);
+  });
+
+  ingredientsDiv.appendChild(ingredientsList);
+  knowMoreDiv.appendChild(ingredientsDiv);
+  knowMoreDiv.appendChild(directionsDiv);
+
+  container.appendChild(knowMoreDiv);
+}
+
+const displayReviews = (recipe, container) => {
+  // console.log(`Displaying reviews for: ${recipe.name}`);
+
+  let commentSectionDiv = document.createElement("section");
+  let reviewCounter = document.createElement("h2");
+
+  reviewCounter.textContent = `Reviews: (${recipe.reviews.length})`;
+  commentSectionDiv.appendChild(reviewCounter);
+
+
+  recipe.reviews.forEach((review) => {
+    let commentDiv = document.createElement("div");
+    let commentAuthor = document.createElement("p");
+    let commentRating = document.createElement("p");
+    let commentDate = document.createElement("p");
+    let comment = document.createElement("p");
+
+    commentAuthor.textContent = review.name;
+    commentRating.textContent = `Rating: ${review.commentRating}`;
+    commentDate.textContent = `Date: ${review.date}`;
+    comment.textContent = `${review.comment}`;
+    commentDiv.appendChild(commentAuthor);
+    commentDiv.appendChild(commentRating);
+    commentDiv.appendChild(commentDate);
+    commentDiv.appendChild(comment);
+    commentDiv.classList.add("comment");
+    commentSectionDiv.appendChild(commentDiv);
+  });
+
+  container.appendChild(commentSectionDiv);
 }
 
 function clearButtons() {
